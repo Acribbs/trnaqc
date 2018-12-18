33114b53e12901870d0f0a1b15463102e2ebda21
Anna James-Bott <anna.james-bott@st-hildas.ox.ac.uk>
2018-12-18 13:55:15 +0000

    Ordered by total coverage of cluster

--- R/genome_coverage.R
+++ R/genome_coverage.R
@@ -1,5 +1,6 @@
 library("tidyverse")
 library("ggpubr")
+library("forcats")
 setwd("/ifs/research-groups/botnar/proj025/analyses/test_trna_3")
 
 file_1 <- read_tsv(file = "genome_statistics.dir/test.genomecov", col_names = FALSE)
@@ -10,14 +11,14 @@ colnames(file_2) <- c("cluster", "total_read_length", "total_coverage", "not_sur
 combined <- left_join(file_1,file_2, by = "cluster")
 combined$percentage_coverage <- ifelse(combined$total_coverage > 0, (combined$coverage / combined$total_coverage )*100, 0)
 
+sorted_combined <- arrange(combined, plyr::desc(total_coverage))
+sorted_combined <- sorted_combined %>% dplyr::filter(cluster %in% head(unique(sorted_combined$cluster),50))
+sorted_combined$cluster <- factor(sorted_combined$cluster, levels = unique(sorted_combined$cluster))
 
-## 
-# Just using 50 unique clusters, need to find top 50 differentially expressed
-small_combined <- combined %>% dplyr::filter(cluster %in% head(unique(combined$cluster),50))
-
-subset_combined <- dplyr::select(small_combined, -c(not_sure)) 
-coverage_plot <- ggplot(data = subset_combined, aes(x= position, y = cluster)) + 
-  geom_point(aes(colour = percentage_coverage)) +theme_bw() + labs(x= "Position in tRNA", y = "Cluster") + scale_colour_gradient(low= "white", high = "blue")
+coverage_plot <- ggplot(data = sorted_combined, aes(x= position, y = fct_reorder(cluster, total_coverage, desc= TRUE))) + 
+  geom_point(aes(colour = percentage_coverage)) +theme_bw() + labs(x= "Position in tRNA", y = "Cluster") +
+  scale_colour_gradient(low= "white", high = "blue") + xlim(0, max(sorted_combined$total_read_length)) + 
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour= "black"))
 print(coverage_plot)
-
-# Need to incorporate DESeq2 and set for highest 50 differentially expressed not just top 50 listed
+# Need to incorporate DESeq2 and set for highest 50 differentially expressed
+# Using total coverage and ranking by read depth of cluster overall
